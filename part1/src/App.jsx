595527a8177d63cbdef2e07febaba917c096007c
Aaron Place <60446365+batskywalker@users.noreply.github.com>
2026-05-16 15:56:39 -0700

    whatever bro

---
+++ part1/src/App.jsx
@@ -0,0 +1,26 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
+
+  return (
+    <div>
+      <h1>Greeting</h1>
+
+      <Hello/>
+      <Hello/>
+      <Hello/>
+    </div>
+  )
+}
+
+export default App
